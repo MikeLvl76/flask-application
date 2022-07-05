c42484ca6b05a885c5a4440af1722ad7b3ca17fa
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-05 10:26:44 +0200

    page redirection with button

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello(name):
     return render_template('hello.html', name=name)
