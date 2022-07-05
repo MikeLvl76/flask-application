6b51a069b572062fc88262ba867f6b90d8bbe9f1
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-05 19:44:14 +0200

    form for sorting list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask import render_template
+from copy import deepcopy
 
 app = Flask(__name__)
 
@@ -13,9 +14,11 @@ def hello():
         result = request.form
         return render_template('hello.html', result=result)
 
-@app.route('/sort/')
-@app.route('/sort/<text>')
-def sort(text):
-    array = text.strip('][').split(', ')
-    list.sort(array)
-    return render_template('sort.html', unsort=text, sorted=array)
+@app.route('/sort', methods = ['POST', 'GET'])
+def sort():
+    if request.method == 'POST':
+        formPage = request.form
+        array = [formPage.get(f"v{i}") for i in range(1, 6)]
+        start = deepcopy(array)
+        list.sort(array)
+        return render_template('sort.html', unsort=start, form=formPage, sorted=array)
